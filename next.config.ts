b39dff0c331c15bb373352e6ab0794672e75906f
Z User <z@container>
2026-05-26 08:32:34 +0000

    fix: permanently exclude examples/ from Vercel build - tsconfig exclude + Sentry ignore

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,12 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
+  experimental: {
+    serverExternalPackages: [],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
   silent: true,
+  ignore: ["examples/**"],
 });
